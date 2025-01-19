d08243280e2419c5ab8786756f55593e37c673fb
David ADLER <david.adler@outlook.com>
2025-01-19 16:49:31 +0100

    feat: auto update

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+
   networking.hostName = "homenas"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
