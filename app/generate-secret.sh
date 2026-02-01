d65fa6bc2f4e022c16ee001c5ca7843a693affa2
David ADLER <david.adler@outlook.com>
2026-02-01 17:03:05 +0100

    update generate-secret script

--- app/generate-secret.sh
+++ app/generate-secret.sh
@@ -13,9 +13,14 @@ generate_file_if_no_exit() {
     fi
 }
 
-BASE_PATH_SECRETS="$(dirname "$0")/secrets"
-BASE_PATH_KEY="$(dirname "$0")/key"
-CN="nas.local"
+BASE_PATH="$(dirname "$0")/secrets/test"
+CN="nas-test.local"
+
+echo "base path : ${BASE_PATH}"
+echo "cn : ${CN}"
+
+BASE_PATH_SECRETS="${BASE_PATH}/secrets"
+BASE_PATH_KEY="${BASE_PATH}/key"
 
 mkdir -p "$BASE_PATH_SECRETS/authelia"
 mkdir -p "$BASE_PATH_KEY/authelia"
@@ -24,13 +29,10 @@ generate_random_string  64 > "$BASE_PATH_SECRETS/authelia/jwt_secret"
 generate_random_string  64 > "$BASE_PATH_SECRETS/authelia/session_secret"
 generate_random_string  64 > "$BASE_PATH_SECRETS/authelia/oidc_hmac_secret"
 generate_random_string  64 > "$BASE_PATH_SECRETS/authelia/oicd_nextcloud_secret"
+generate_random_string  64 > "$BASE_PATH_SECRETS/authelia/storage_encryption_key"
 
-# generate_file_if_no_exit "$BASE_PATH_SECRETS/authelia/ldap_password"
-ssh root@ssh.nas.local -C 'cat /etc/nixos/secrets/olcRootPW'  > app/secrets/authelia/ldap_password
+ssh root@ssh."${CN}" -C 'cat /etc/nixos/secrets/olcRootPW'  > "$BASE_PATH_SECRETS/authelia/ldap_password"
 
-echo  you_must_generate_a_random_string_of_more_than_twenty_chars_and_configure_this > "$BASE_PATH_SECRETS/authelia/storage_encryption_key"
-
-
-openssl req -x509 -nodes -newkey rsa:2048 -keyout "$BASE_PATH_KEY/authelia/private.pem" -out public.crt -sha256 -days 365 -subj "/CN=$CN"
+openssl req -x509 -nodes -newkey rsa:2048 -keyout "$BASE_PATH_KEY/authelia/private.pem" -out "$BASE_PATH_KEY/authelia/public.crt" -sha256 -days 365 -subj "/CN=$CN"
 openssl genpkey -algorithm RSA -out "$BASE_PATH_KEY/authelia/private_key.pem" -pkeyopt rsa_keygen_bits:2048
 openssl req -new -x509 -key "$BASE_PATH_KEY/authelia/private_key.pem" -out "$BASE_PATH_KEY/authelia/certificate.pem" -days 365 -subj "/CN=$CN"
