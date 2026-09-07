c6af1202d941002ef1afb14ee77d4d8f2f717b70
David ADLER <david.adler@outlook.com>
2026-09-07 15:04:06 +0200

    secure jellyfin

--- service/jellyfin.nix
+++ service/jellyfin.nix
@@ -1,10 +1,67 @@
 {
   config,
+  pkgs,
   secrets,
   vars,
   ...
 }:
 {
+  fileSystems."/srv/jellyfin/videos" = {
+    device = "/data/2/videos";
+    fsType = "none";
+    options = [ "bind" "ro" ];
+    depends = [ "/data/2" ];
+  };
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      libva-vdpau-driver
+    ];
+  };
+
+  users.users.jellyfin.extraGroups = [
+    "render"
+    "video"
+  ];
+
+  systemd.services.jellyfin.serviceConfig = {
+    ProtectSystem = "strict";
+    ProtectHome = "tmpfs";
+    PrivateTmp = true;
+    PrivateMounts = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectControlGroups = true;
+    LockPersonality = true;
+    RestrictSUIDSGID = true;
+    NoNewPrivileges = true;
+
+    ReadWritePaths = [
+      "/var/lib/jellyfin"
+      "/var/cache/jellyfin"
+      "/var/log/jellyfin"
+    ];
+    BindReadOnlyPaths = [ "/srv/jellyfin" ];
+    InaccessiblePaths = [
+      "/data"
+      "/srv/borg"
+      "/sftp"
+      "/home"
+    ];
+    TemporaryFileSystem = "/:ro";
+
+    PrivateDevices = false;
+    DevicePolicy = "closed";
+    DeviceAllow = [
+      "/dev/dri/renderD128 rw"
+      "/dev/dri/card0 rw"
+      "char-drm rw"
+    ];
+  };
+
   services.jellyfin = {
     enable = true;
   };
